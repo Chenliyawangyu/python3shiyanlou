802a43321e986cd0eba36a1737c694d3454a015b
chenliya <1102446204@qq.com>
2023-04-21 22:20:42 +0800

    python3

---
+++ python3shiyanlou1.py
@@ -0,0 +1,8 @@
+
+if __name__ == '__python3shiyanlou1__':
+    a = 1
+    for a in range(1, 101):
+        if a % 7 == 0 or a % 10 == 7 or a // 10 == 7:
+            continue
+        else:
+            print(a)
